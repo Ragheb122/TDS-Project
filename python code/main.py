a3c20e2a189932471fd56d51621c8281e8a79e6f
raghebghazi <raghep-11@hotmail.com>
2023-03-08 20:06:41 +0200

    python files added

--- python code/main.py
+++ python code/main.py
@@ -1,5 +1,5 @@
 import warnings
-from datasets_handling import vgsales_handling, titanic_handling, StudentsPerformance_handling, Mart_Sale_Forecast_handling
+from datasets_handling import *
 from empirical_experiments_utils import RMSE
 from plotting_utils import plot1, plot2, org_distribution_graph, after_distribution_graph
 warnings.filterwarnings('ignore')
@@ -10,26 +10,26 @@ plot1(df_original, df, idx, key)
 RMSE(y_actual, [df_original[key].mean()] * len(y_actual), 'Mean')
 plot2(df_original[key].mean(), idx, df, key)
 
-# dataset 2
-df_original, df, idx, y_actual, y_predicted, key = StudentsPerformance_handling('')
-RMSE(y_actual, y_predicted, 'Linear Regression')
-plot1(df_original, df, idx, key)
-RMSE(y_actual, [df_original[key].mean()] * len(y_actual), 'Mean')
-plot2(df_original[key].mean(), idx, df, key)
-
-# dataset 3
-df_plot_org, df_plot_after, y_actual, y_predicted, most_freq = Mart_Sale_Forecast_handling('')
-RMSE(y_actual, y_predicted, 'Random Forest')
-org_distribution_graph(df_plot_org, 'Outlet_Location_Type')
-RMSE(y_actual, [most_freq] * len(y_actual), 'Most Frequent')
-after_distribution_graph(df_plot_after, 'Most Frequent')
-
-# dataset 4
-df_plot_org, df_plot_after, y_actual, y_predicted, most_freq = titanic_handling('')
-RMSE(y_actual, y_predicted, 'Random Forest')
-org_distribution_graph(df_plot_org, 'Embarked')
-RMSE(y_actual, [most_freq] * len(y_actual), 'Most Frequent')
-after_distribution_graph(df_plot_after, 'Most Frequent')
+# # dataset 2
+# df_original, df, idx, y_actual, y_predicted, key = StudentsPerformance_handling('')
+# RMSE(y_actual, y_predicted, 'Linear Regression')
+# plot1(df_original, df, idx, key)
+# RMSE(y_actual, [df_original[key].mean()] * len(y_actual), 'Mean')
+# plot2(df_original[key].mean(), idx, df, key)
+#
+# # dataset 3
+# df_plot_org, df_plot_after, y_actual, y_predicted, most_freq = Mart_Sale_Forecast_handling('')
+# RMSE(y_actual, y_predicted, 'Random Forest')
+# org_distribution_graph(df_plot_org, 'Outlet_Location_Type')
+# RMSE(y_actual, [most_freq] * len(y_actual), 'Most Frequent')
+# after_distribution_graph(df_plot_after, 'Most Frequent')
+#
+# # dataset 4
+# df_plot_org, df_plot_after, y_actual, y_predicted, most_freq = titanic_handling('')
+# RMSE(y_actual, y_predicted, 'Random Forest')
+# org_distribution_graph(df_plot_org, 'Embarked')
+# RMSE(y_actual, [most_freq] * len(y_actual), 'Most Frequent')
+# after_distribution_graph(df_plot_after, 'Most Frequent')
 
 
 
